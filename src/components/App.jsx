6f21b932c42dfe62d48c7e0f50a1e851612c1d16
corey redding <coreysnightout@gmail.com>
2017-08-26 23:54:59 -0700

    add UserFormControls component to App.jsx component so it will be ready for building out form for users of app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import styles from '../styles/App.css';
 import succulent from '../assets/test.png';
+import UserFormControls from './UserFormControls';
 
 function App(props){
   var appStyles = {
-    fontFamily: "sans-serif"
+    fontFamily: "sans-serif",
+    display: "flex"
   }
   return (
     <div style={appStyles}>
       <h1>The Budding</h1>
       <img src={succulent} alt="succulent example image" />
+      <div>
+        <UserFormControls/>
+      </div>
     </div>
   );
 }
---
+++ src/components/UserFormControls.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function UserFormControls(props){
+
+
+
+  return (
+    <div>
+      <p>test!</p>
+    </div>
+  );
+}
+
+export default UserFormControls;
